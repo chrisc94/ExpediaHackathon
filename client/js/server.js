944d059e4550964be310fd0a421847810ec1ee2c
chang47 <coralbue@gmail.com>
2016-01-31 11:32:17 -0800

    added server data

--- client/js/server.js
+++ client/js/server.js
@@ -15,14 +15,7 @@ $(document).ready(function() {
 		});
 	}
 
-	var object = {body: [{a: "a", b: 1}, {a: "b", b: 2}]};
-	var json = JSON.parse(JSON.stringify(object));
-	console.log(json);
-	$('#postcall').click(function() {
-		$.post("https://obscure-temple-85981.herokuapp.com/", json, function(data) {
-			console.log(data);
-		})
-	});
+	
 	console.log("word: " + $.urlParam('a'));
 	$('#getcall').click(function() {
 		$.get("https://obscure-temple-85981.herokuapp.com/find/56ade3fa895b8d8c0adb2b0e", function(data) {
