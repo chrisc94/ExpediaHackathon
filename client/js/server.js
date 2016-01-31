3609b6606f42cad924283cb0d4296d21bc99bce4
chang47 <coralbue@gmail.com>
2016-01-31 12:40:48 -0800

    asdsad

--- client/js/server.js
+++ client/js/server.js
@@ -14,11 +14,18 @@ $(document).ready(function() {
 		console.log("load");
 		$.get("https://obscure-temple-85981.herokuapp.com/find/" + a, function(data) {
 			console.log(data);
+			var cost = 0;
 			for (var i = 0; i < data.body.length; i++) {
 				var body = data.body[i];
 				$("#myTrip").append('<button type="button" class="list-group-item text-left"><span class="score badge">' + body.parse + '</span>' +' <span class="score badge">'+ body.badge  +"</span>" + body.title + '</button>');	
+				console.log(body.parse);
+				var price = parseInt(body.parse.replace('$', ''));
+				cost += price;
 			}
-     
+			if (isNaN(cost)) {
+				cost = 0
+			}
+     		$('#costs').text("$" + cost + '! What a great deal!');
 		});
 	}
 
